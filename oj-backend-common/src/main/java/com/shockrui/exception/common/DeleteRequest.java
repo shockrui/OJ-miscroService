b2836d98bc532432a3d0e7b86957a93954b7d46b
shockrui <1206640240@qq.com>
2024-04-17 21:23:31 +0800

    oj项目拆分微服务初始化

---
+++ oj-backend-common/src/main/java/com/shockrui/exception/common/DeleteRequest.java
@@ -0,0 +1,22 @@
+package com.shockrui.exception.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 删除请求
+ *
+ * @author shockRui
+ * @from 
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ oj-backend-user-service/src/test/java/com/shockrui/ojbackenduserservice/OjBackendUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shockrui.ojbackenduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjBackendUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
